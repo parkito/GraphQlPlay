4aff6ebd45abc0935eb9e83e4424ef9c4f932bcf
parkito <artyom-karnov@yandex.ru>
2019-05-03 19:59:13 +0300

    Add graphql schemas

--- server/src/main/kotlin/com/graphql/play/server/graphql/EventController.kt
+++ server/src/main/kotlin/com/graphql/play/server/graphql/EventController.kt
@@ -4,6 +4,6 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import org.springframework.stereotype.Component
 
 @Component
-class Event : GraphQLQueryResolver {
+class EventController : GraphQLQueryResolver {
     fun version() = "1.0.0"
 }
